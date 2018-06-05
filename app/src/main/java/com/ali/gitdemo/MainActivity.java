e317dc43ea99c1f09cc4969ec7740d46a55dcb0f
wangjiangtao <tongchexinfeitao@sina.cn>
2018-06-05 16:08:42 +0800

    规范“

---
+++ app/src/main/java/com/ali/gitdemo/MainActivity.java
@@ -0,0 +1,27 @@
+package com.ali.gitdemo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+//大驼峰 每一个单词首字母都是大写
+public class MainActivity extends AppCompatActivity {
+
+    //小驼峰  第一个单词首字母小写  如果是成员变量命名可以以m开头
+    private TextView mLoginTv;
+    //静态的成员变量，用s开头
+    private static String sName;
+    //如果是全局的可以用g开头
+    public static int gAge;
+    //常量的命名是全部大写，单词之间用下划线分割
+    public static final int MAX_AGE = 100;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mLoginTv = findViewById(R.id.tv_login);
+    }
+}
